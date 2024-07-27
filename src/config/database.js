c9fc47d49cd53b0771a1ef8a8fd65ab1eb160f13
ThanaponJaiauea <ham@yojoies.com>
2024-07-27 14:17:16 +0700

    api

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+/** @format */
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "123456",
+    database: "projectBooking",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: "123456",
+    database: "projectBooking",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: "123456",
+    database: "projectBooking",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+}
