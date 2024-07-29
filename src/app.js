2a0652bea149818bff285860c91409741bbfc081
ThanaponJaiauea <162958446+ThanaponJaiauea@users.noreply.github.com>
2024-07-29 09:11:14 +0700

    ##

--- src/app.js
+++ src/app.js
@@ -4,33 +4,33 @@
 // sequelize.sync({force: true})
 // sequelize.sync({ alter: true });
 
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const chalk = require("chalk")
-const morgan = require("morgan")
-const helmet = require("helmet")
-const rateLimit = require("express-rate-limit")
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
-const authRoute = require("./routes/auth-route")
+const authRoute = require("./routes/auth-route");
 
-const app = express()
+const app = express();
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(
   rateLimit({
     windowMs: 1000 * 60 * 15,
     max: 1000,
-    message: {message: "to many requests, please try again later"},
+    message: { message: "to many requests, please try again later" },
   })
-)
-app.use(helmet())
-app.use(cors())
-app.use(express.json())
+);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
 
-const port = 8888 || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(chalk.yellowBright.italic.bold`server runnig on port: ${port}`)
-})
+  console.log(chalk.yellowBright.italic.bold`server runnig on port: ${port}`);
+});
--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,25 @@
-/** @format */
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "123456",
-    database: "projectBooking",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: "123456",
-    database: "projectBooking",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: "123456",
-    database: "projectBooking",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
   },
-}
+};
